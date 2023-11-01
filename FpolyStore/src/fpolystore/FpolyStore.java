dad26fb3580e10b32a7624ab75e113227df6e4f2
thonghtph43253 <hoangthong2105@gmail.com>
2023-11-01 19:29:43 +0700

    test

---
+++ FpolyStore/src/fpolystore/FpolyStore.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fpolystore;
+
+/**
+ *
+ * @author Admin
+ */
+public class FpolyStore {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hoàng trung thông");
+    }
+    
+}
