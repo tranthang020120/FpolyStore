cf9678f4a5eebbb1b0fd4edb5df9fe6d2c2ac44b
thonghtph43253 <hoangthong2105@gmail.com>
2023-11-12 19:30:55 +0700

    upmodel chatlieu

--- FpolyStore/src/com/fstore/model/ChatLieu.java
+++ FpolyStore/src/com/fstore/model/ChatLieu.java
@@ -9,5 +9,51 @@ package com.fstore.model;
  * @author Admin
  */
 public class ChatLieu {
+    private int ID_ChatLieu;
+    private String tenChatLieu;
+    private int trangThai;
+
+    public ChatLieu() {
+    }
+
+    public ChatLieu(int ID_ChatLieu, String tenChatLieu, int trangThai) {
+        this.ID_ChatLieu = ID_ChatLieu;
+        this.tenChatLieu = tenChatLieu;
+        this.trangThai = trangThai;
+    }
+    public ChatLieu( String tenChatLieu, int trangThai) {
+        
+        this.tenChatLieu = tenChatLieu;
+        this.trangThai = trangThai;
+    }
+
+    public int getID_ChatLieu() {
+        return ID_ChatLieu;
+    }
+
+    public void setID_ChatLieu(int ID_ChatLieu) {
+        this.ID_ChatLieu = ID_ChatLieu;
+    }
+
+    public String getTenChatLieu() {
+        return tenChatLieu;
+    }
+
+    public void setTenChatLieu(String tenChatLieu) {
+        this.tenChatLieu = tenChatLieu;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    @Override
+    public String toString() {
+        return "ChatLieu{" + '}';
+    }
     
 }
